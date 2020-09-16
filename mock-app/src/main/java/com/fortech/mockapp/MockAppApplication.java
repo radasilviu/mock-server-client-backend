fb1100bb29c64ba6dc15e3150c9e00a42ac59e80
Vinter Antonio <tonyivinter@gmail.com>
2020-09-16 13:45:59 +0300

    added

---
+++ mock-app/src/main/java/com/fortech/mockapp/MockAppApplication.java
@@ -0,0 +1,13 @@
+package com.fortech.mockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockAppApplication.class, args);
+	}
+
+}
