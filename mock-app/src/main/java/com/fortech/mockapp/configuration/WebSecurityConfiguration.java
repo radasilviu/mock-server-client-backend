834bbe320730d4f1fc8a1ee7f5a254a2a76b5b31
Robert Kis <robert.kis@fortech.ro>
2020-09-18 12:18:27 +0300

    Enabled global CORS policy for frontend App

--- mock-app/src/main/java/com/fortech/mockapp/configuration/WebSecurityConfiguration.java
+++ mock-app/src/main/java/com/fortech/mockapp/configuration/WebSecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.fortech.mockapp.configuration;
 
 import com.fortech.mockapp.configuration.filters.AuthorizationServerAccessFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
@@ -15,16 +18,30 @@ import java.util.Arrays;
 
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private Environment env;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors()
+        http
+            .cors()
                 .and()
             .csrf().disable()
             .addFilterBefore(new AuthorizationServerAccessFilter(), SecurityContextPersistenceFilter.class)
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(this.env.getProperty("frontedRootURL")));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- mock-app/src/main/java/com/fortech/mockapp/controllers/RequiredAuthAppController.java
+++ mock-app/src/main/java/com/fortech/mockapp/controllers/RequiredAuthAppController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:4200")
 public class RequiredAuthAppController {
 
     AppService appService;
