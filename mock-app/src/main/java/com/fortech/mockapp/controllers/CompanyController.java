7bedc03cc7a4b8abb73b44ef78d47e637e655489
Robert Kis <robert.kis@fortech.ro>
2020-09-22 16:50:09 +0300

    Moved controller implementation to service

--- mock-app/src/main/java/com/fortech/mockapp/controllers/CompanyController.java
+++ mock-app/src/main/java/com/fortech/mockapp/controllers/CompanyController.java
@@ -8,9 +8,6 @@ import com.fortech.mockapp.response.GeneralResponse;
 import com.fortech.mockapp.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -26,17 +23,7 @@ public class CompanyController {
     @PostMapping(path="/list")
     public @ResponseBody
     CompanyListResponse getAllCompanies(@RequestBody CompanyListRequest request) {
-        int pageNumber = request.getOffset() / request.getLimit();
-        Sort sort = null;
-
-        if (request.getSortDirection().equals("desc")) {
-            sort = Sort.by(request.getSortColumn()).descending();
-        } else {
-            sort = Sort.by(request.getSortColumn()).ascending();
-        }
-
-        Pageable page = PageRequest.of(pageNumber, request.getLimit(), sort);
-        Page<CompanyModel> companies = companyRepository.findAll(page);
+        Page<CompanyModel> companies = companyService.list(request);
         CompanyListResponse response = new CompanyListResponse(companies.getTotalElements(), companies.getContent());
         return response;
     }
--- mock-app/src/main/java/com/fortech/mockapp/service/CompanyService.java
+++ mock-app/src/main/java/com/fortech/mockapp/service/CompanyService.java
@@ -2,7 +2,12 @@ package com.fortech.mockapp.service;
 
 import com.fortech.mockapp.model.CompanyModel;
 import com.fortech.mockapp.repository.CompanyRepository;
+import com.fortech.mockapp.request.CompanyListRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -21,5 +26,20 @@ public class CompanyService {
 
     public void delete(int id) {
         companyRepository.deleteById(id);
+    }
+
+    public Page<CompanyModel> list(CompanyListRequest request) {
+        int pageNumber = request.getOffset() / request.getLimit();
+        Sort sort = null;
+
+        if (request.getSortDirection().equals("desc")) {
+            sort = Sort.by(request.getSortColumn()).descending();
+        } else {
+            sort = Sort.by(request.getSortColumn()).ascending();
+        }
+
+        Pageable page = PageRequest.of(pageNumber, request.getLimit(), sort);
+        Page<CompanyModel> companies = companyRepository.findAll(page);
+        return companies;
     }
 }
