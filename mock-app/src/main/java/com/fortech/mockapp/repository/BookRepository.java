da58b745387e7cb5db93fd3066080ecfcd94fe57
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-09-22 10:11:40 +0300

    Book repository updated

--- mock-app/src/main/java/com/fortech/mockapp/repository/BookRepository.java
+++ mock-app/src/main/java/com/fortech/mockapp/repository/BookRepository.java
@@ -1,7 +1,10 @@
 package com.fortech.mockapp.repository;
 
 import com.fortech.mockapp.entities.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(String title, Pageable pageable);
 }
