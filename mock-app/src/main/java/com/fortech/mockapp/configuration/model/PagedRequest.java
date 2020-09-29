b1719e600f2f5f79c0237ea30645da3ce8e6f21a
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-09-29 12:19:39 +0300

    Added constructor to PagedRequest class

--- mock-app/src/main/java/com/fortech/mockapp/configuration/model/PagedRequest.java
+++ mock-app/src/main/java/com/fortech/mockapp/configuration/model/PagedRequest.java
@@ -14,5 +14,17 @@ public class PagedRequest {
     private String sortDirection;
     private String sortColumn;
     private ArrayList<String> columnsToSearchIn;
+
+    public PagedRequest(Integer pageSize, Integer offset, String searchTerm, String sortDirection, String sortColumn, ArrayList<String> columnsToSearchIn) {
+        this.pageSize = pageSize;
+        this.offset = offset;
+        this.searchTerm = searchTerm;
+        this.sortDirection = sortDirection;
+        this.sortColumn = sortColumn;
+        this.columnsToSearchIn = columnsToSearchIn;
+    }
+
+    public PagedRequest() {
+    }
 }
 
