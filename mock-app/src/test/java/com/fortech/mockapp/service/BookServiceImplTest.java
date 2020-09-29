bad44b6b8cc4617e3f5168f5211450f0ff45970c
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-09-29 11:34:14 +0300

    Added Mockito dependency in pom

---
+++ mock-app/src/test/java/com/fortech/mockapp/service/BookServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.fortech.mockapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookServiceImplTest {
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void deleteBookById() {
+    }
+
+    @Test
+    void getBookPagedResponse() {
+    }
+}
