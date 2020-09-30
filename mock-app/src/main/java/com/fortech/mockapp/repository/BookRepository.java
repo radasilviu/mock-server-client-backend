29a412eea7edb946dfa73def38f4e7918a1a6483
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-09-30 16:41:02 +0300

    Updated repositories to extend a common interface

--- mock-app/src/main/java/com/fortech/mockapp/repository/BookRepository.java
+++ mock-app/src/main/java/com/fortech/mockapp/repository/BookRepository.java
@@ -1,13 +1,12 @@
 package com.fortech.mockapp.repository;
 
+import com.fortech.mockapp.DynamicallySearchableRepository;
 import com.fortech.mockapp.entities.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BookRepository extends JpaRepository<Book, String>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends DynamicallySearchableRepository<Book, String> {
       Page<Book> findAll(Specification bookSpecification, Pageable paging);
 }
 
--- mock-app/src/main/java/com/fortech/mockapp/repository/CompanyRepository.java
+++ mock-app/src/main/java/com/fortech/mockapp/repository/CompanyRepository.java
@@ -1,15 +1,17 @@
 package com.fortech.mockapp.repository;
 
+import com.fortech.mockapp.DynamicallySearchableRepository;
 import com.fortech.mockapp.model.CompanyModel;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.transaction.Transactional;
 
 @Repository
 @Transactional
 public interface CompanyRepository extends
-        PagingAndSortingRepository<CompanyModel, Integer>, JpaSpecificationExecutor<CompanyModel> {
+        DynamicallySearchableRepository<CompanyModel, Integer> {
 
 }
