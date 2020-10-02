5acaaae6f9899cd91f24e14898c90e33e42d9bf8
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-10-02 15:05:17 +0300

    Something with unit test

--- mock-app/src/test/java/com/fortech/mockapp/service/BookServiceImplTest.java
+++ mock-app/src/test/java/com/fortech/mockapp/service/BookServiceImplTest.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 
@@ -25,10 +26,10 @@ class BookServiceImplTest {
     private BookRepository bookRepository;
 
     @InjectMocks
-    private BookService bookService = new BookServiceImpl(bookRepository);
+    private final BookService bookService = new BookServiceImpl(bookRepository);
 
     // Book entity has a public Id setter (which is stupid),
-    // but how could we check the ID stays the same if we set it private?
+    // but how could we check the ID stays the same if we set it private (thus forcing it to be null)?
     // - Not that we'd really need to
     @Test
     void whenUpdatingBookShouldFetchBookWithMatchingIdFromRepositoryAndUpdateItAndSaveIt() {
@@ -63,14 +64,36 @@ class BookServiceImplTest {
         PagedRequest requestParams = new PagedRequest(
                 10, 0, "term", "asc", "title", columnsToSearchIn
         );
-        ArrayList<Book> bookList = new ArrayList<>();
-        bookList.add(new Book());
-        Pageable pageable = PageRequest.of(requestParams.getPageNumber(), requestParams.getPageSize(), Sort.by("title").ascending());
-        Page<Book> expected = new PageImpl<>(bookList, pageable, 100);
-        Mockito.when(bookRepository.findAll((Specification) any(), (Pageable) any())).thenReturn(expected);
 
-        bookService.getBookPagedResponse(requestParams);
+        Page<Book> returnedPage = getBookPage(requestParams);
+        Mockito.when(bookRepository.findAll((Specification) any(), (Pageable) any())).thenReturn(returnedPage);
+
+        bookService.getPagedResponse(requestParams);
 
         Mockito.verify(bookRepository).findAll((Specification) any(), (Pageable) any());
+    }
+
+    @Test
+    void whenGettingBookPagedResponse_ShouldSortByColumnTitleIfNotOtherwiseMentioned() {
+        ArrayList<String> columnsToSearchIn = new ArrayList<>();
+        columnsToSearchIn.add("");
+        PagedRequest requestParams = new PagedRequest(
+                10, 0, "term", "asc", "title", columnsToSearchIn
+        );
+        Page<Book> expectedPage = getBookPage(requestParams);
+        Mockito.when(bookRepository.findAll((Specification) any(), (Pageable) any())).thenReturn(expectedPage);
+
+        Map<String, Object> actual = bookService.getPagedResponse(requestParams);
+
+        Assert.assertEquals(actual, actual);
+    }
+
+    private Page<Book> getBookPage(PagedRequest requestParams) {
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(new Book());
+
+        Pageable pageable = PageRequest.of(requestParams.getPageNumber(), requestParams.getPageSize(), Sort.by("title").ascending());
+
+        return new PageImpl<>(bookList, pageable, 100);
     }
 }
