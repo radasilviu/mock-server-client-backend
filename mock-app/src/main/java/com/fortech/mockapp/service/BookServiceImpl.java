bf2b881f110fc206e4acd35b666445173e00dc91
Sebastian-Ionut Sirb <sebastian.sirb@fortech.ro>
2020-10-06 15:15:29 +0300

    Added Exception throwing to updateBook method

--- mock-app/src/main/java/com/fortech/mockapp/service/BookServiceImpl.java
+++ mock-app/src/main/java/com/fortech/mockapp/service/BookServiceImpl.java
@@ -1,10 +1,12 @@
 package com.fortech.mockapp.service;
 
+import com.fortech.mockapp.DataLayerException;
 import com.fortech.mockapp.Pager;
 import com.fortech.mockapp.configuration.model.PagedRequest;
 import com.fortech.mockapp.entities.Book;
 import com.fortech.mockapp.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 
@@ -19,11 +21,14 @@ public class BookServiceImpl implements BookService{
     }
 
     @Override
-    public void updateBook(String bookId, Book book) {
-        Book bookToUpdate = bookRepository.getOne(bookId);
-        bookToUpdate.setAuthor(book.getAuthor());
-        bookToUpdate.setPrice(book.getPrice());
-        bookToUpdate.setTitle(book.getTitle());
+    public void updateBook(String bookId, Book newBook) throws DataLayerException {
+        Book bookToUpdate = bookRepository.findById(bookId)
+                .orElseThrow(() -> new DataLayerException(
+                "Could not find book with that ID", HttpStatus.NOT_FOUND
+        ));
+        bookToUpdate.setAuthor(newBook.getAuthor());
+        bookToUpdate.setPrice(newBook.getPrice());
+        bookToUpdate.setTitle(newBook.getTitle());
         bookRepository.save(bookToUpdate);
     }
 
